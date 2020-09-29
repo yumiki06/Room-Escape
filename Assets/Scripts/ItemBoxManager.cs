b7c922eadfcde5beb6078e67b7fad37979bbe0bb
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-09-29 14:49:21 +0900

    Create ItemBoxManager

--- Assets/Scripts/ItemBoxManager.cs
+++ Assets/Scripts/ItemBoxManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemBox : MonoBehaviour
+public class ItemBoxManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
