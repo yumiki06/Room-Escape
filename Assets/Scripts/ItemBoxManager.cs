3630c3c855dcaf699778e7a566a5b49f5af072af
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-09-29 21:52:45 +0900

    Add field
    パネルで編集できるようにする為

--- Assets/Scripts/ItemBoxManager.cs
+++ Assets/Scripts/ItemBoxManager.cs
@@ -12,6 +12,13 @@ public enum ITEM
 
 public class ItemBoxManager : MonoBehaviour
 {
+    [SerializeField] Sprite roomKeySprite; //鍵の画像
+    [SerializeField] Image[] itemBoxImages;
+
+    ITEM[] itemList = new ITEM[4]; //取得したアイテムの配列
+
+    // アイテムを取得
+    public void SetItem(ITEM item)
     {
     }
     {
